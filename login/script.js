92029f0b453935d2b0381b1a1df24ee605e60c0a
RyusukeHo <rh3016512@gmail.com>
2024-08-05 19:59:24 +0900

    Update script.js

--- login/script.js
+++ login/script.js
@@ -12,27 +12,6 @@ let onLogin = async () => {
     login.disabled = true;
     welcome.innerHTML = "ログインしています";
     message.innerHTML = "<p>お待ちください...</p><svg width=\"30\" height=\"30\" viewBox=\"-60 -60 120 120\"><circle r =\"50\" /></svg>";
-    fetchPost();
-    /*
-    let response = await fetch("https://script.google.com/macros/s/AKfycbwzB6H3Wy_V1MK_TMSkxBi3Kusz2MBEtuRkphpA7w9DKS9ApQOcaO-HH-Yh8mtEmQxy/exec?id=" + encodeURIComponent(id.value) + "&password=" + encodeURIComponent(password.value), {
-        method: 'GET'
-    })
-    let obj = await response.json();
-    if (obj.code == "Success") {
-        successLogin(obj.body);
-    } else if (obj.code == "Failure") {
-        failureLogin("パスワードが間違っています。");
-    } else if (obj.code == "NotFound") {
-        failureLogin("アカウントが見つかりませんでした。");
-    }
-        */
-};
-async function fetchPost() {
-    //DriveApp.getRootFolder().getName(); //ウェブアプリ実行時に４０１エラーが出た場合はこれを実行すると良さそう 
-    //ウェブアプリを全体公開出来ない場合はトークンが必要。下の行のコメントアウトをはずし、トークンをfetch直前に取得
-    //const token = ScriptApp.getOAuthToken();
-
-    //送りたいデータをここで設定。
     const data = {
         "id": id.value,
         "password": password.value
@@ -43,7 +22,6 @@ async function fetchPost() {
         'method': 'post',
 
         'headers': {
-            'Content-Type': 'application/json'
             //,'Authorization': `Bearer ${token}` //ウェブアプリを全体公開出来ない場合この認証が必要なので、コメントアウトをはずす。
         },
         'body': JSON.stringify(data) //送りたいデータをpayloadに配置してJSON形式変換。
@@ -57,7 +35,7 @@ async function fetchPost() {
     } else if (obj.code == "NotFound") {
         failureLogin("アカウントが見つかりませんでした。");
     }
-}
+};
 let failureLogin = (reason) => {
     welcome.innerHTML = "もう一度お試しください";
     message.innerHTML = "<p class=\"error\"><span class=\"dli-exclamation-circle\"><span></span></span>" + reason + "</p><p>パスワードまたはIDをお忘れの場合は<a href=\"https://docs.google.com/forms/d/1wNEdvV9nDlvMgC66nMCSAnSKapXjBu6Itvs4cufiJWQ/viewform?edit_requested=true\">こちら</a></p> ";
